253d8eb5157f3080053dd7470b5c89765bb7246e
Sp0derDev <Sp0derDev@protonmail.com>
2024-04-20 04:37:08 +0300

    created homepage

--- app/page.js
+++ app/page.js
@@ -1,43 +1,25 @@
-"use client";
-import { useState, useEffect } from "react";
-import Pusher from "pusher-js";
+import { WavyBackground } from "@/components/ui/wavy-background";
+import Link from "next/link";
 
 const HomePage = () => {
-  const [sound, setSound] = useState("Silence");
-  const [category, setCategory] = useState("Listening");
-
-  useEffect(() => {
-    var pusher = new Pusher("a23354f7a99fcd36d314", { cluster: "ap2" });
-    const actionsChannel = pusher.subscribe("actions");
-    const classificationChannel = pusher.subscribe("classification");
-    // actionsChannel.bind("sound", (data) => {
-    //   const { message } = data;
-    //   console.log("Sound:", message);
-    //   setSound(message);
-
-    //   setInterval(() => {
-    //     setSound("Silence");
-    //   }, 2);
-    // });
-    classificationChannel.bind("category", (data) => {
-      const { message } = data;
-      console.log("Category:", message);
-      setCategory(message);
-      console.log("Meow");
-    });
-  }, []);
-
   return (
-    <div>
-      <h1>Sound Detected: {category}</h1>
-      <button
-        type="button"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        onClick={() => setCategory("Silence")}
-      >
-        Reset
-      </button>
-    </div>
+    <WavyBackground className="max-w-4xl mx-auto pb-40">
+      <p className="text-2xl text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+        Decibel AI
+      </p>
+      <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+        Leverage the power of Machine Learning to detect sound and classify it
+      </p>
+
+      <div className="flex justify-center mt-8">
+        <Link
+          href={"/listen"}
+          className="bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Start Listening
+        </Link>
+      </div>
+    </WavyBackground>
   );
 };
 
